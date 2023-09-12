15843d3f9c52c9cf69f602aa4b086c982b514332
thomasAmara <thomastarawali@gmail.com>
2023-09-12 22:36:43 +1000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+import React, { Suspense, lazy } from 'react';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+const AboutUs = lazy(() => import('./Pages/AboutUs'));
+const Home = lazy(() => import('./Pages/Home'));
+const FAQ = lazy(() => import('./Pages/FAQ'));
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Router>
+        <Suspense fallback={null}>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/Aboutus' element={<AboutUs />} />
+            <Route path='/faq' element={<FAQ />} />
+          </Routes>
+        </Suspense>
+      </Router>
     </div>
   );
 }
