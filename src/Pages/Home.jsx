d8d06f59dae279c41b701ad7bdb983f190b63df1
Apatakayuss <apatakayuss@gmail.com>
2023-09-16 19:35:43 +0100

    Footer two

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
-import Footer from '../Components/Footer'
-function Home() {
-  return <div>Home Page
 
-<Footer />
-  </div>;
+function Home() {
+  return (
+    <div className='HomeContainer'>
+      Home Page
+      <div className='summitContainer'>
+        <text style={{ color: '#222222', fontWeight: '700', fontSize: '24px' }}>
+          EKO City Summit 2023
+        </text>
+        <div>
+          <div>text</div>
+          <div>Image</div>
+        </div>
+      </div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
 }
 
 export default Home;
