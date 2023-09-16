cdd0e86ddd62e9209c9d73c87780b03b0821ed52
thomasAmara <thomastarawali@gmail.com>
2023-09-16 22:06:59 +1000

    Home page

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,7 +1,24 @@
 import React from 'react';
 
 function Home() {
-  return <div>Home Page</div>;
+  return (
+    <div className='HomeContainer'>
+      Home Page
+      <div className='summitContainer'>
+        <text style={{ color: '#222222', fontWeight: '700', fontSize: '24px' }}>
+          EKO City Summit 2023
+        </text>
+        <div>
+          <div>text</div>
+          <div>Image</div>
+        </div>
+      </div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
 }
 
 export default Home;
