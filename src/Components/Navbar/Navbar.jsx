9f1b1d0d40d1ddb33db89fef8e2ba75d0a3c1c2c
thomasAmara <thomastarawali@gmail.com>
2023-09-22 23:23:41 +1000

    font added

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,43 +1,50 @@
-import React from 'react'
-import "../Navbar/Navbar.css"
-import logo from "../../../src/Images/LCS 2 1 (1).svg"
-import { Link } from 'react-router-dom'
-import AboutUs from '../../Pages/AboutUs'
-import {BiSolidDownArrow} from "react-icons/bi"
-import {BiRightArrowAlt} from "react-icons/bi"
-import FAQ from '../../Pages/FAQ/FAQ'
+import React from "react";
+import "../Navbar/Navbar.css";
+import logo from "../../../src/Images/LCS 2 1 (1).svg";
+import { Link } from "react-router-dom";
+import AboutUs from "../../Pages/AboutUs";
+import { BiSolidDownArrow } from "react-icons/bi";
+import { BiRightArrowAlt } from "react-icons/bi";
+import FAQ from "../../Pages/FAQ/FAQ";
 
 export default function Navbar() {
   return (
-    <div className='navbar'>
+    <div className="navbar">
       <div className="navbar-nav">
-      <img src={logo} alt="" />
-      <div className='navbar-pages'>
-        <Link to="/AboutUs">About Us</Link>
-        <Link>Agenda</Link>
-        <div className="navbar-svg">
-          <Link>Speakers</Link>
-          <BiSolidDownArrow size={10}/>
+        <Link to="/">
+          <img src={logo} alt="" />
+        </Link>
+        <div className="navbar-pages">
+          <Link to="/AboutUs">About Us</Link>
+          <Link>Agenda</Link>
+          <div className="navbar-svg">
+            <Link>Speakers</Link>
+            <BiSolidDownArrow size={10} />
+          </div>
+          <div className="navbar-svg">
+            <Link to="/Sponsors">Sponsors</Link>
+            <BiSolidDownArrow size={10} />
+          </div>
+          <div className="navbar-svg">
+            <Link to="/FAQ">Summit FAQ</Link>
+            <BiSolidDownArrow size={10} />
+          </div>
+          <Link>Blogs</Link>
+          <Link>Gallery</Link>
         </div>
-        <div className="navbar-svg">
-          <Link>Sponsors</Link>
-          <BiSolidDownArrow size={10}/>
+        <div className="book-now">
+          <button>
+          <Link to="./BookNow">Book Now
+            Book Now <BiRightArrowAlt color="white" /></Link>
+          </button>
         </div>
-        <div className="navbar-svg">
-          <Link to="/FAQ">Summit FAQ</Link>
-          <BiSolidDownArrow size={10}/>
-        </div>
-        <Link>Blogs</Link>
-        <Link>Gallery</Link>
-      </div>
-      <div className="book-now">
-        <button>Book Now <BiRightArrowAlt color='white'/></button>
-      </div>
       </div>
       <div className="nav-btn">
-        <button>Book Today</button>
+        <button>
+          <Link to=''>Book Today</Link>
+        </button>
         <button>Sponsor Enquiry</button>
       </div>
     </div>
-  )
+  );
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router, Switch, 
-  Route, Redirect,} from "react-router-dom";
+import { BrowserRouter as Router} from "react-router-dom";
 import App from './App';
+import './Fonts/Poppins/Poppins-Regular.ttf'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
