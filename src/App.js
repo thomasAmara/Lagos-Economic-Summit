2553c85ef33a3a63c083a0bd450d519cd3597209
thomasAmara <thomastarawali@gmail.com>
2023-09-25 22:16:40 +1000

    route path for speakers

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React, { Suspense, lazy } from 'react';
-import './App.css';
-import { Routes, Route } from 'react-router-dom';
-import Navbar from './Components/Navbar/Navbar';
-const AboutUs = lazy(() => import('./Pages/AboutUs'));
-const Home = lazy(() => import('./Pages/Home'));
-const FAQ = lazy(() => import('./Pages/FAQ/FAQ'));
-const BookNow = lazy(() => import('./Pages/Book'));
-const Sponsors = lazy(() => import('./Pages/Sponsors'));
+import React, { Suspense, lazy } from "react";
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./Components/Navbar/Navbar";
+const AboutUs = lazy(() => import("./Pages/AboutUs"));
+const Home = lazy(() => import("./Pages/Home"));
+const FAQ = lazy(() => import("./Pages/FAQ/FAQ"));
+const BookNow = lazy(() => import("./Pages/Book"));
+const Sponsors = lazy(() => import("./Pages/Sponsors"));
+const Speakers = lazy(() => import("./Pages/Speaker"));
 
 function App() {
   return (
     <div className='App'>
       <Navbar />
-        <Suspense fallback={null}>
-          <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/Aboutus' element={<AboutUs />} />
-            <Route path='/FAQ' element={<FAQ />} />
-            <Route path='/BookNow' element={<BookNow />}/>
-            <Route path='/Sponsors' element={<Sponsors />}/>
-          </Routes>
-        </Suspense>
+      <Suspense fallback={null}>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/Aboutus' element={<AboutUs />} />
+          <Route path='/FAQ' element={<FAQ />} />
+          <Route path='/BookNow' element={<BookNow />} />
+          <Route path='/Sponsors' element={<Sponsors />} />
+          <Route path='/Speakers' element={<Speakers />} />
+        </Routes>
+      </Suspense>
     </div>
   );
 }
