3cd8833f5593e38f7b8198dfe7cd41ad02b6afe8
Apatakayuss <apatakayuss@gmail.com>
2023-09-27 11:36:19 +0100

    Footer fixed

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -11,27 +11,36 @@ const Footer = () => {
 return (
     <div className='footer'>
         <div className='detail'>
-            <div className="detail-left deets">
+            <div className="detail-left deets" style={{display: 'flex', flexDirection: 'column'}}>
                 <h4 style={{ color: '#0F3879' }}>Office Address</h4>
                 <p className='p-mid'>Sunny Motors, Plot 3, Otunba Adedoyin Ogunge close,</p>
                 <p>Lekki phase 1, Lagos. Nigeria.</p>
+
+
+                <div className='info-img' style={{justifyContent: 'flex-start'}}>
+                <img src={Powered} alt="Adanian" />
             </div>
-            <div className="detail-right deets">
+            </div>
+            <div className="detail-right deets" style={{display: 'flex', flexDirection: 'column'}}>
                 <h4 style={{ color: '#0F3879' }}>Event Address</h4>
                 <p className='p-mid'>The Ball-Room, Lagos Oriental Hotel Lekki - Epe Expy,</p>
                 <p>Victoria Island 106104, Lagos</p>
+
+                <div className='info-tel' style={{justifyContent: 'flex-start'}}>
+                <p style={{ textAlign: 'left', color: '#0F3879' }}>+234 706 728 1296</p>
+            </div>
             </div>
         </div>
 
-        <div className='info'>
-            <div className='info-img'>
+        {/* <div className='info'>
+            <div className='info-img' style={{justifyContent: 'flex-start'}}>
                 <img src={Powered} alt="Adanian" />
             </div>
 
-            <div className='info-tel' style={{textAlign: 'left'}}>
+            <div className='info-tel' style={{justifyContent: 'flex-start'}}>
                 <p style={{ textAlign: 'left', color: '#0F3879' }}>+234 706 728 1296</p>
             </div>
-        </div>
+        </div> */}
 
         <hr />
         <div className="foot">
@@ -40,16 +49,16 @@ return (
             </div>
             <div className='foot-icon'>
 
-            <a href="/" class="iconlink" target="_blank">
+            <a href="/" class="iconlink" target="_blank" rel='noreferrer'>
             <AiFillMail />
             </a>
-            <a href="/" class="iconlink" target="_blank">
+            <a href="/" class="iconlink" target="_blank" rel='noreferrer'>
             <FaFacebookF />
             </a>
-            <a href="https://twitter.com/OfficialApata" class="iconlink" target="_blank">
+            <a href="https://twitter.com/OfficialApata" class="iconlink" target="_blank" rel='noreferrer'>
             <AiOutlineTwitter />
             </a>
-            <a href="/" class="iconlink" target="_blank">
+            <a href="/" class="iconlink" target="_blank" rel='noreferrer'>
             <AiOutlineInstagram />
             </a>
             <a href="/" class="iconlink" target="_blank">
