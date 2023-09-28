640bb6e1baf34bd8884470d059d69a60167229a6
marisalex <101093957+marisalex@users.noreply.github.com>
2023-09-28 10:51:06 +0100

    added alt text

--- src/Pages/Blog/Blog.jsx
+++ src/Pages/Blog/Blog.jsx
@@ -8,7 +8,18 @@ export default function Blog() {
       <div className="blog-content">
         <h1>ESG SUMMIT NEWS</h1>
         <div className="blog-list">
-          <img src={plant} />
+          <img src={plant} alt=""/>
+          <div className="blog-details">
+            <h3>
+              Work with the Ecosystem on Environmental, Social and Governance.
+            </h3>
+            <p className="read-more" alt="">Read More</p>
+            <p className="date">October 18th, 2023</p>
+          </div>
+        </div>
+
+        <div className="blog-list">
+          <img src={plant} alt=""/>
           <div className="blog-details">
             <h3>
               Work with the Ecosystem on Environmental, Social and Governance.
@@ -19,18 +30,7 @@ export default function Blog() {
         </div>
 
         <div className="blog-list">
-          <img src={plant} />
-          <div className="blog-details">
-            <h3>
-              Work with the Ecosystem on Environmental, Social and Governance.
-            </h3>
-            <p className="read-more">Read More</p>
-            <p className="date">October 18th, 2023</p>
-          </div>
-        </div>
-
-        <div className="blog-list">
-          <img src={plant} />
+          <img src={plant} alt=""/>
           <div className="blog-details">
             <h3>
               Work with the Ecosystem on Environmental, Social and Governance.
