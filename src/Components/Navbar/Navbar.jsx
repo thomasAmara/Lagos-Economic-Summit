ac05006cbde96f9204c0b4f297a7e5b222f75ee4
thomasAmara <thomastarawali@gmail.com>
2023-10-16 22:39:05 +1100

    new changes

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,52 +1,91 @@
 import React from "react";
 import "../Navbar/Navbar.css";
 import logo from "../../../src/Images/LCS 2 1 (1).svg";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
+import { Link, Element } from "react-scroll";
 // import AboutUs from "../../Pages/AboutUs";
-import { BiSolidDownArrow } from "react-icons/bi";
+import { Text } from "@chakra-ui/react";
 // import { BiRightArrowAlt } from "react-icons/bi";
 // import FAQ from "../../Pages/FAQ/FAQ";
 // import Blog from "../../Pages/Blog/Blog";
 
 export default function Navbar() {
+  const handleSetActive = (to) => {
+    console.log("destination", to);
+  };
   return (
-    <div className='navbar'>
-      <div className='navbar-nav'>
-        <Link to='/'>
-          <img src={logo} alt='' />
-        </Link>
-        <div className='navbar-pages'>
-          <Link to='/AboutUs'>About Us</Link>
-          <div className='navbar-svg'>
-            <Link to='/Speakers'>Speakers</Link>
-            <BiSolidDownArrow size={10} color='#0F3879' />
-          </div>
-          <div className='navbar-svg'>
-            <Link to='/Sponsors'>Sponsors</Link>
-            <BiSolidDownArrow size={10} color='#0F3879' />
-          </div>
-          <div className='navbar-svg'>
-            <Link to='/FAQ'>Summit FAQ</Link>
-            <BiSolidDownArrow size={10} color='#0F3879' />
-          </div>
-          <Link to='/Blog'>Blogs</Link>
-          <Link>Gallery</Link>
-        </div>
-        <div className='book-now'>
-          <button>
-            <Link
-              to='./BookNow'
-              style={{
-                color: "#FFF",
-                fontSize: "18px",
-              }}
-            >
-              Book Now
+    <div className='navbar sticky-navbar'>
+      <div className='navbar'>
+        <div className='navbar-nav'>
+          <div style={{ padding: "10px 0px" }}>
+            <Link to='/'>
+              <img src={logo} alt='' />
             </Link>
-          </button>
+          </div>
+          <div className='navbar-pages'>
+            <div className='navbar-svg'>
+              <Link to='/AboutUs' smooth={true} duration={500}>
+                <Text
+                  fontSize='15px'
+                  fontWeight='600'
+                  onSetActive={handleSetActive}
+                >
+                  About
+                </Text>{" "}
+              </Link>
+            </div>
+
+            <div className='navbar-svg'>
+              <Link to='/Speakers' smooth={true} duration={500}>
+                <Text
+                  fontSize='15px'
+                  fontWeight='600'
+                  onSetActive={handleSetActive}
+                >
+                  Speakers
+                </Text>
+              </Link>
+            </div>
+            <div className='navbar-svg'>
+              <Link to='/Sponsors' smooth={true} duration={500}>
+                <Text
+                  fontSize='15px'
+                  fontWeight='600'
+                  onSetActive={handleSetActive}
+                >
+                  Sponsors
+                </Text>
+              </Link>
+            </div>
+            <div className='navbar-svg'>
+              <Link to='/Agenda' smooth={true} duration={500}>
+                <Text fontSize='15px' fontWeight='600'>
+                  Agenda
+                </Text>
+              </Link>
+            </div>
+            {/* <Link to='/Blog'> */}
+            <Text fontSize='15px' fontWeight='600'>
+              Practical Information
+            </Text>
+            {/* </Link> */}
+            {/* <Link>Gallery</Link> */}
+          </div>
+          <div className='book-now'>
+            <button>
+              <Link
+                to='./BookNow'
+                style={{
+                  color: "#FFF",
+                  fontSize: "18px",
+                }}
+              >
+                Book Now
+              </Link>
+            </button>
+          </div>
         </div>
-      </div>
-      <div className='nav-btn'>
+        {/* <div className='nav-btn'>
         <button>
           <Link
             to='/BookNow'
@@ -65,6 +104,13 @@ export default function Navbar() {
         >
           Sponsor Enquiry
         </button>
+      </div> */}
+        <Element name='Aboutus'>
+          Ko test 23<div style={{ height: "60vh" }}>dfko</div>
+        </Element>
+        <Element name='Agenda' className='element'>
+          test 1<div style={{ height: "60vh" }}>dfko</div>
+        </Element>
       </div>
     </div>
   );
