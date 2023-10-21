81138425bdb74a45e764f50b1d2ccd71c06304d4
thomasAmara <thomastarawali@gmail.com>
2023-10-22 00:16:58 +1100

    presenter component

--- src/App.js
+++ src/App.js
@@ -7,12 +7,13 @@ import Footer from "./Components/Footer";
 // const AboutUs = lazy(() => import("./Pages/AboutUs"));
 // const Home = lazy(() => import("./Pages/Home"));
 const Sponsors = lazy(() => import("./Pages/Sponsors"));
-// const Speakers = lazy(() => import("./Pages/Speaker"));
+const Presenter = lazy(() => import("./Pages/Presenter"));
 const About = lazy(() => import("./Components/About/About"));
 const Summit = lazy(() => import("./Components/SummitNumber/SummitNumber"));
-const SpeakerOutput = lazy(() => import("./Components/SpeakerOutput/SpeakerOutput"));
+const SpeakerOutput = lazy(() =>
+  import("./Components/SpeakerOutput/SpeakerOutput")
+);
 // const Agenda = lazy(() => import("./Components/Agenda/Agenda"));
-
 
 function App() {
   return (
@@ -20,19 +21,19 @@ function App() {
       <div style={{ position: "sticky" }}>
         <Navbar />
         <Element name='AboutUs'>
-          <div style={{ height: "100%"}}>
+          <div style={{ height: "100%" }}>
             <About />
           </div>
         </Element>
 
-       <Element name='SummitNumber'>
-         <div style={{ height: "100%"}}>
+        <Element name='SummitNumber'>
+          <div style={{ height: "100%" }}>
             <Summit />
           </div>
         </Element>
 
         <Element name='Speakers'>
-          <div style={{ height: "100%", padding: '20px 0' }}>
+          <div style={{ height: "100%", padding: "20px 0" }}>
             <SpeakerOutput />
           </div>
         </Element>
@@ -44,12 +45,16 @@ function App() {
         </Element>
 
         <Element name='Sponsors'>
-          <div style={{ height: "100%", backgroundColor: "#EEE32D" }}>
-            <h1>Sponsors Section</h1>
+          <div style={{ height: "100%" }}>
             <Sponsors />
           </div>
         </Element>
-        
+        <Element name='Sponsors'>
+          <div style={{ height: "100%" }}>
+            <Presenter />
+          </div>
+        </Element>
+
         <Footer />
       </div>
     </div>
